22536e6f987a6ce593bc9366a32841ca28cfa137
jsmalex <jsmalejandro@hotmail.com>
2024-05-21 15:18:25 +0100

    Version con la mosca inicialmente colocada en uno de los dos lados posibles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let canvas = document.getElementById("canvas");
 let miMosca;
-console.log(canvas.style)
+
 
 function comenzarJuego(){
     miMosca = new Mosca();
-    miMosca.insertarMosca(canvas);
+    miMosca.insertarMosca();
 }
 
 comenzarJuego();
--- js/mosca.js
+++ js/mosca.js
@@ -9,26 +9,27 @@ class Mosca{
                 
     }
     
-    insertarMosca(canvas){
+    insertarMosca(){
         this.boton.setAttribute('class','mosca');
        
-        this.dimeLado(canvas);
+        this.dimeLado();
         this.boton.style.width = this.width + 'px';
         this.boton.style.height = this.height + 'PX';
         canvas.appendChild(this.boton);
         
     }
 
-    dimeLado(canvas){
+    dimeLado(){
       let ceroOUno = Math.round(Math.random());
       console.log(ceroOUno)
       //si lado es true la mosca sale a izquierda y si es false sale a derecha
       if (ceroOUno) {
-        console.log(canvas);
-        let puntoIzq = canvas.style.height - this.boton.style.height;
-        console.log(puntoIzq);
-        this.boton.style.top = puntoIzq + "px";
         
+        this.boton.style.top = 450 + "px";
+        
+      }else{
+        this.boton.style.top = 450 + "px";
+        this.boton.style.left = 750 + "px";
       }
     }
 }   
